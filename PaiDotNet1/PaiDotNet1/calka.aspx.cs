cf7c50d9ebb70b35a3e9c384990256fc3ea63a5a
Aithlin <wikita228@gmail.com>
2017-01-25 19:30:05 +0100

    watki

--- PaiDotNet1/PaiDotNet1/calka.aspx.cs
+++ PaiDotNet1/PaiDotNet1/calka.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -18,19 +19,36 @@ namespace PaiDotNet1
         {
             double a = double.Parse(TextBox1.Text);
             double b = double.Parse(TextBox2.Text);
+            int liczbaWatkow = 8;
             
-            Label1.Text = sinus(a,b).ToString();
+            Label1.Text = sinus2(a,b,liczbaWatkow).ToString();
         }
 
-        private double sinus(double a, double b)
+        private double sinus2(double a, double b, int liczbaWatkow)
         {
             int dokladnosc = 1000;
+            double przedzial = (b - a) / liczbaWatkow;
+            double[] wynik = new double[liczbaWatkow];
+
+            Parallel.For(0, liczbaWatkow, (int x) =>
+             {
+                 double start = a + x * przedzial;
+                 double koniec = start + przedzial;
+
+                 wynik[x] = sinus(start, koniec, dokladnosc);
+             });
+
+            return wynik.Sum();
+        }
+
+        private double sinus(double a, double b, double dokladnosc = 1000)
+        {
             double h = (b - a) / dokladnosc;
             double wynik = 0.0;
 
             for(double x=a;x<b;x+=h)
             {
-                wynik += trapez(Math.Sin(x), Math.Sin(x + h), h);
+                wynik += trapez(Math.Sin(x)-Math.Cos(x), Math.Sin(x + h)- Math.Cos(x + h), h);
             }
 
             return wynik;
