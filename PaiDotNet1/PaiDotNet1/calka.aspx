827e2c2a926386c951ce7ba26c4e832a1537c701
Aithlin <wikita228@gmail.com>
2017-01-25 18:56:14 +0100

    init, podstawowy sinus

---
+++ PaiDotNet1/PaiDotNet1/calka.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="calka.aspx.cs" Inherits="PaiDotNet1.calka" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>
+        <br />
+    </p>
+    <form id="form1" runat="server">
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ PaiDotNet1/PaiDotNet1/calka.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PaiDotNet1
+{
+    public partial class calka : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(TextBox1.Text);
+            double b = double.Parse(TextBox2.Text);
+            
+            Label1.Text = sinus(a,b).ToString();
+        }
+
+        private double sinus(double a, double b)
+        {
+            int dokladnosc = 1000;
+            double h = (b - a) / dokladnosc;
+            double wynik = 0.0;
+
+            for(double x=a;x<b;x+=h)
+            {
+                wynik += trapez(Math.Sin(x), Math.Sin(x + h), h);
+            }
+
+            return wynik;
+        }
+
+        private double trapez(double a, double b, double h)
+        {
+            return (a + b) * h / 2;
+        }
+    }
+}
