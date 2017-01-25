53fa953703e0edc1ff9a026123ef164456f38ff0
Aithlin <wikita228@gmail.com>
2017-01-25 22:35:37 +0100

    wykres

--- PaiDotNet1/PaiDotNet1/calka.aspx
+++ PaiDotNet1/PaiDotNet1/calka.aspx
@@ -1,5 +1,7 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="calka.aspx.cs" Inherits="PaiDotNet1.calka" %>
 
+<%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -24,6 +26,17 @@
             <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
         </p>
     <div>
+    
+        <asp:Chart ID="Chart1" runat="server">
+            <series>
+                <asp:Series Name="Series1">
+                </asp:Series>
+            </series>
+            <chartareas>
+                <asp:ChartArea Name="ChartArea1">
+                </asp:ChartArea>
+            </chartareas>
+        </asp:Chart>
     
     </div>
     </form>
--- PaiDotNet1/PaiDotNet1/calka.aspx.cs
+++ PaiDotNet1/PaiDotNet1/calka.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -17,11 +18,48 @@ namespace PaiDotNet1
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            Chart1.Visible = true;
+            Chart1.Series.Clear();
+            Chart1.Titles.Clear();
+            
+            Chart1.Titles.Add("Całka sinx");
+            Chart1.ChartAreas[0].AxisX.Title = "Oś wartości X";
+            Chart1.ChartAreas[0].AxisY.Title = "Oś wartości Y";
+            Chart1.ChartAreas[0].AxisY.LineWidth = 5;
+            
+            Chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.Black;
+            Chart1.ChartAreas[0].AxisX.MinorGrid.Enabled = true;
+            Chart1.ChartAreas[0].AxisX.MinorGrid.LineDashStyle = System.Web.UI.DataVisualization.Charting.ChartDashStyle.Dot;
+            
+            Chart1.ChartAreas[0].AxisY.MinorGrid.Enabled = true;
+            Chart1.ChartAreas[0].AxisY.MinorGrid.LineColor = Color.Silver;
+            Chart1.ChartAreas[0].AxisY.MinorGrid.LineDashStyle = System.Web.UI.DataVisualization.Charting.ChartDashStyle.Dot;
+            
+            Chart1.Series.Add("Wykres sinx");
+
+            Chart1.Series["Wykres sinx"].ChartType = System.Web.UI.DataVisualization.Charting.SeriesChartType.Line;
+            Chart1.Series["Wykres sinx"].Color = Color.DarkBlue;
+            Chart1.Series["Wykres sinx"].BorderWidth = 2;
+
+            Chart1.Series.Add("Wykres cosx");
+
+            Chart1.Series["Wykres cosx"].ChartType = System.Web.UI.DataVisualization.Charting.SeriesChartType.Line;
+            Chart1.Series["Wykres cosx"].Color = Color.DarkOrange;
+            Chart1.Series["Wykres cosx"].BorderWidth = 2;
+
+
             double a = double.Parse(TextBox1.Text);
             double b = double.Parse(TextBox2.Text);
             int liczbaWatkow = 8;
             
             Label1.Text = sinus2(a,b,liczbaWatkow).ToString();
+
+
+            for (double x = a; x <= b; x += 0.001)
+            {
+                Chart1.Series["Wykres sinx"].Points.AddXY(x, Math.Sin(x));
+                Chart1.Series["Wykres cosx"].Points.AddXY(x, Math.Cos(x));
+            }
         }
 
         private double sinus2(double a, double b, int liczbaWatkow)
